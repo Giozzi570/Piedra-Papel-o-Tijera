5ceab79b1d4b67b00ae79232d51c9d7586c2c572
Federico Giozzi <fedegiozzi@gmail.com>
2025-02-20 13:56:03 -0300

    cambios

--- script.js
+++ script.js
@@ -3,40 +3,87 @@ const juego = document.getElementById("juego")
 const piedra = document.getElementById("piedraUser")
 const tijeras = document.getElementById("tijerasUser")
 const papel = document.getElementById("papelUser")
-const ruleta = document.getElementById("ruleta")
 const body = document.querySelector("body")
-
-
+const userValue = document.getElementById("userValue")
+const elementos = document.querySelectorAll("#piedraUser, #papelUser, #tijerasUser");
+const valueUser = document.getElementById("valueUser")
+const maquinaValue = document.getElementById("maquinaValue")
+let user;
+let maquina;
+let arrayButtons = [papel,tijeras,piedra]
 const numeroAleatorio = () =>{
     numero = Math.random() * 10000
     console.log(numero)
 }
-
-
+elegirAleatorio = (array) =>{
+    const indice = Math.floor(Math.random() * array.length);
+    console.log(indice)
+    return array[indice];
+}
 iniciarJuego.addEventListener("click", () => {
     iniciarJuego.style.display = "none"
     juego.style.display = "flex"
 })
-
+const eliminateElementValueUser = () =>{
+    valueUser.style.display = "none"
+}
 piedra.addEventListener("click", () => {
-    ruleta.style.transition = "transform 1s ease-in-out"
+    const opciones = ["Piedra", "Papel", "Tijeras"];
+    maquina = elegirAleatorio(opciones);
     numeroAleatorio()
-    ruleta.style.transform = `rotate(${numero}deg)`
-    
+    user = "piedra"
+    verificarImagenUser()
+    eliminateElementValueUser()
+    setTimeout(verificarImagenMaquina(),2000)
 })
 papel.addEventListener("click", () => {
-    ruleta.style.transition = "transform 1s ease-in-out"
+    const opciones = ["Piedra", "Papel", "Tijeras"];
+    maquina = elegirAleatorio(opciones);
     numeroAleatorio()
-    ruleta.style.transform = `rotate(${numero}deg)`
+    user = "papel"
+    verificarImagenUser()
+    eliminateElementValueUser()
+    setTimeout(verificarImagenMaquina(),2000)
 })
 tijeras.addEventListener("click", () => {
-    ruleta.style.transition = "transform 1s ease-in-out"
+    const opciones = ["piedra", "papel", "tijeras"];
+    maquina = elegirAleatorio(opciones);
     numeroAleatorio()
-    ruleta.style.transform = `rotate(${numero}deg)`
+    user = "tijeras"
+    verificarImagenUser()
+    eliminateElementValueUser()
+    setTimeout(verificarImagenMaquina(),2000)
 })
 
+const verificarImagenUser = () => {
+    switch (user){
+        case "piedra":
+            userValue.setAttribute("src","static/piedra.png")
+            console.log("piedra")
+            break
+        case "papel":
+            userValue.setAttribute("src","static/papel.png")
+            console.log("papel")
+            break
+        case "tijeras":
+            userValue.setAttribute("src","static/tijeras.png")
+            break
+    }
 
+}
+const verificarImagenMaquina = () => {
+    switch (maquina){
+        case "piedra":
+            maquinaValue.setAttribute("src","static/piedra.png")
+            console.log("piedra")
+            break
+        case "papel":
+            maquinaValue.setAttribute("src","static/papel.png")
+            console.log("papel")
+            break
+        case "tijeras":
+            maquinaValue.setAttribute("src","static/tijeras.png")
+            break
+    }
 
-
-
-
+}
